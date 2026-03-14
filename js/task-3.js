fd08e942715375ef7842f1ac5c048fbbe3c0a764
plashchynskyi-dev <plaschinskiyandriy@gmail.com>
2026-03-14 19:42:38 +0200

    hw done

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,6 @@
-
+function getElementWidth(content , padding , border ) {
+  const contentWidth = Number.parseFloat(content);
+  const paddingWidth = Number.parseFloat(padding);
+  const borderWidth = Number.parseFloat(border);
+  return contentWidth + paddingWidth * 2 + borderWidth * 2;
+}
